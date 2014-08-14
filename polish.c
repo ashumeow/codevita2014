da75040fc14c68be73d7eb3ec70cdc8cc9b28f1f
Aswini S <ashumeow@live.com>
2014-08-14 22:15:46 +0530

    Create polish.c

---
+++ polish.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define test_cases T[100]
+int main()
+{
+int T, N;
+N[500] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+for(N=1; N <= T; N++)
+{
+printf("Enter T which is less than 100 \n");
+scanf("%d", &T);
+if(1<=T<=100)
+printf("yes \n");
+else
+printf("no \n");
+}
+return 0;
+}
+
